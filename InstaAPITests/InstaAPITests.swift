28fd924aba84a6140acd344fff71851e6f79f899
KumagaiNaoki <j.a.r.n0405@gmail.com>
2017-02-27 11:57:23 +0900

    Initial Commit

---
+++ InstaAPITests/InstaAPITests.swift
@@ -0,0 +1,36 @@
+//
+//  InstaAPITests.swift
+//  InstaAPITests
+//
+//  Created by KumagaiNaoki on 2017/02/27.
+//  Copyright © 2017年 KumagaiNaoki. All rights reserved.
+//
+
+import XCTest
+@testable import InstaAPI
+
+class InstaAPITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
