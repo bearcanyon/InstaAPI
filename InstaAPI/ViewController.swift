3a1584fee206093016af1a4b6fe73e72418e174f
KumagaiNaoki <j.a.r.n0405@gmail.com>
2017-02-28 14:06:19 +0900

    ライブラリ導入
    - Alamofire
    - SwiftyJSON

--- InstaAPI/ViewController.swift
+++ InstaAPI/ViewController.swift
@@ -7,19 +7,22 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+//https://instagram.com/oauth/authorize/?client_id=0c301ed49ab945bcb9bc0011f1c1cd3a&redirect_uri=https://www.facebook.com/naokikumagai0405&response_type=token
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//https://instagram.com/oauth/authorize/?client_id=0c301ed49ab945bcb9bc0011f1c1cd3a&redirect_uri=http://insta-api.jp&response_type=token
     }
-
+//    https://instagram.com/oauth/authorize/?client_id=0c301ed49ab945bcb9bc0011f1c1cd3a&redirect_uri=https://www.facebook.com/naokikumagai0405&response_type=token
 
 }
 
+//返って来たアクセストークン
+//https://www.facebook.com/naokikumagai0405#access_token=30724986.0c301ed.31740b81f98f43f6a6c4217bb7182a26
+
+//AlamofireとSwiftyJSONを使用する
